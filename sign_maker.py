f1d7532a3b8746c163a786e829244a166e14b854
abc <abc@gmail.com>
2025-08-06 23:33:33 +0530

    updated a line in the sign_make.py

--- sign_maker.py
+++ sign_maker.py
@@ -1,5 +1,5 @@
 # sign_maker.py
 # This version is super simple to test if Python is working.
 with open('index.html', 'w') as f:
-    f.write("<h1>Hellow there</h1><p>This is a test.</p>")
+    f.write("<h1>Hellow there yup</h1><p>This is a test.</p>")
 print("Test sign (index.html) created successfully!")
